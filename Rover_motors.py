7d76ba70f667794e6be9b051b7a416de0cb2e7ff
andrei-popovici <popoviciandrei@protonmail.com>
2023-12-16 23:59:39 +0200

    hehe

--- Rover_motors.py
+++ Rover_motors.py
@@ -7,12 +7,15 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 front_right360 = 0
+
 front_right180 = 5
 
+front_left180 = 7
 
 
 for i in range(6,12):
   kit.servo[i].set_pulse_width_range(servo180_1, servo180_2)
 
 while True:
-  kit.continuous_servo[0].throttle = 1
+  kit.continuous_servo[front_right360].throttle = 1
+  kit.servo[front_left180].angle = 90
