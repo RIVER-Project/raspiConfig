6121c9f78c24cf22b3408b598189322aada3aac0
andrei-popovici <popoviciandrei@protonmail.com>
2023-12-17 00:03:00 +0200

    hehe

--- Rover_motors.py
+++ Rover_motors.py
@@ -8,6 +8,8 @@ kit = ServoKit(channels=16)
 
 front_right360 = 0
 
+front_left360 = 1
+
 front_right180 = 5
 
 front_left180 = 7
@@ -17,5 +19,5 @@ for i in range(6,12):
   kit.servo[i].set_pulse_width_range(servo180_1, servo180_2)
 
 while True:
-  kit.continuous_servo[front_right360].throttle = 1
-  kit.servo[front_left180].angle = 90
+  kit.continuous_servo[front_left360].throttle = 1
+  kit.servo[front_right180].angle = 90
