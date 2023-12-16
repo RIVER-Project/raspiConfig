ae66036dc9debce434dc95294383b64d5709d76b
andrei-popovici <popoviciandrei@protonmail.com>
2023-12-17 00:11:53 +0200

    aed

--- Rover_motors.py
+++ Rover_motors.py
@@ -15,7 +15,7 @@ for i in range(6,12):
   kit.servo[i].set_pulse_width_range(servo180_1, servo180_2)
 
 while True:
-  kit.continuous_servo[front_right360].throttle = 1
+  kit.continuous_servo[front_right360].throttle = -1
 
   # kit.continuous_servo[front_left360].throttle = 1
   # kit.servo[front_right180].angle = 90
